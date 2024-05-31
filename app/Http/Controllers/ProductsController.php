e9f610a3e06f0f1aaf6316528953063542b65516
ViitorAugusto <vitoraugusto2010201078@gmail.com>
2024-05-31 16:12:16 -0300

    refactor api

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Str;
 
 class ProductsController extends Controller
 {
@@ -16,58 +14,37 @@ class ProductsController extends Controller
     }
 
     public function getProductById($id){
-        if(!$id){
-            return response()->json(["message"=> "id nao fornecido"],400);
+        if (!$id) {
+            return response()->json(["message" => "id nao fornecido"], 400);
         }
-        $product = Product::where('id',$id)->first();
-        if(!$product) return response()->json(["message"=> "produto nao existe"],400);
+        $product = Product::where('id', $id)->first();
+        if (!$product) return response()->json(["message" => "produto nao existe"], 400);
         else return response()->json($product);
     }
 
-    public function store(Request $request){
-        info($request);
+    public function store(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'title' => 'required|string|max:255',
-            'slug' => 'required|string|max:255',
+            'slug' => 'required|string|max:255,unique:products',
             'price' => 'required|numeric',
-            'image' => 'required|file',
-            'description' => 'required|string',
-            'moreDetails' => 'required|string',
-            'featured' => 'required|boolean'
+            'image' => 'nullable|image|max:2048',
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
-
-        $image = $request->file('image');
-        $images = $request->file('images');
-
-        // Process single image
-        $imageName = Str::uuid() . '.' . $image->getClientOriginalExtension();
-        $image->move(public_path('images'), $imageName);
-
-        //Caso tenha apenas uma posicao em 'images', arrume para o código usar isso:
-        $imgName = Str::uuid() . '.' . $images->getClientOriginalExtension();
-        $images->move(public_path('images'), $imgName);
-
-        //Caso tenha várias posições em 'images', arrume para o código usar isso:
-        $imagesName = [];
-        foreach ($images as $img) {
-            $imgName = Str::uuid() . '.' . $img->getClientOriginalExtension();
-            $img->move(public_path('images'), $imgName);
-            $imagesName[] = 'images/' . $imgName;
-        }
+        if ($request->hasFile('image')) {
+        $imagePath = $request->file('image')->store('images', 'public');
+    } else {
+        $imagePath = null;
+    }
 
         $product = Product::create([
             'title' => $request->title,
             'slug' => $request->slug,
             'price' => $request->price,
-            'image' => 'images/' . $imageName,
-            'images' => 'images/' . $imgName,
-            'description' => $request->description,
-            'moreDetails' => $request->moreDetails,
-            'featured' => $request->featured,
+            'image' => $imagePath,
         ]);
 
         return response()->json(['message' => 'Produto criado com sucesso!', 'produto' => $product]);
--- database/migrations/2024_05_24_123226_create_products_table.php
+++ database/migrations/2024_05_24_123226_create_products_table.php
@@ -4,32 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug');
-            $table->integer('price');
-            $table->string('image');
-            $table->string('images');
-            $table->text('description');
-            $table->text('moreDetails');
-            $table->boolean('featured');
+            $table->text('slug');
+            $table->decimal('price', 8, 2);
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('products');
     }
-};
+}
+
